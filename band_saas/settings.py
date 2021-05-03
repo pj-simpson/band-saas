28340d811a865ea0bf78b7f7a919b3f3c7c7d69c
Peter Simpson <peter_joseph_simpson@hotmail.com>
2021-05-03 18:36:00 +0100

    docker reinstated

--- band_saas/settings.py
+++ band_saas/settings.py
@@ -135,14 +135,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if not DEBUG:
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'workspace/static'),
-    )
-else:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "static"),
-    ]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
